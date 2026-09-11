ca828752d4283d4875603e0175f8e54da5540b6d
Prince Mukunzi <p.niyomugab@alustudent.com>
2026-09-11 13:24:32 +0200

    Fix: add missing trailing newline to all C files

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,22 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-  int x;
+int main(void)
+{
+	int x;
 
-  x = 97;
-  while (x < 123) {
-    if (x == 'q' || x == 'e') {
-      x++;
-      continue;
-    }
-    putchar(x);
-    x++;
-  }
-  putchar('\n');
+	x = 97;
+	while (x < 123)
+	{
+		if (x == 'q' || x == 'e')
+		{
+			x++;
+			continue;
+		}
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
